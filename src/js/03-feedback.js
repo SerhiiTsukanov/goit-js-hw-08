ad6ad4f2d5737afdb97a13abd0eeea58b697b04a
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-07 01:05:11 +0300

    task 3
    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+const form = document.querySelector("input", "textarea")
+const submit = document.querySelector("button")
+console.log(form);
+
+
+form.addEventListener("input", throttle(onInput, 500));
+submit.addEventListener("submit", onSubmit);
+
+function onInput(evt) {
+    evt.preventDefault();
+    const message = evt.target.value;
+
+    localStorage.setItem("feedback-form-state", message);
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const {
+        elements: { email, message },
+    } = evt.currentTarget;
+    const formData = { email: email.value, message: message.value };
+    console.log('тправка формы');
+    evt.target.reset("feedback-form-state");
+    localStorage.removeItem()
+}
+
+function populateTextarea() {
+    const saveMessagr = localStorage.getItem("feedback-form-state");
+    if (saveMessagr) {
+        
+    }
+}
