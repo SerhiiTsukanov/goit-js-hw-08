6cb15f0cc479d8f6eda3c25379f66d4df2421231
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-09 23:09:40 +0300

    task 2
    npm install @vimeo/player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
