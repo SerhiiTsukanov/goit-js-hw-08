e5aada31ad882c00d577a919480b146541537830
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-10 00:33:42 +0300

    task 2
    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,20 @@ import Player from '@vimeo/player';
 import '../css/common.css';
 import '../css/03-feedback.css';
 
+const iframe = document.querySelector('#vimeo-player');
 
+const player = new Player(iframe);
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
+const getCurrentTime = localStorage.getItem("feedback-form-state");
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+if (getCurrentTime) {
+
+    // .setCurrentTime - фиксирует плеер на том-же месте
+    player.setCurrentTime(getCurrentTime);
+}
+
+function onPlay({seconds}) {
+localStorage.setItem("feedback-form-state", seconds)
+
+}
+player.on('timeupdate', throttle(onPlay, 1000));
