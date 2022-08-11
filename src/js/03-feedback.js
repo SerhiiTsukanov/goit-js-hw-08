0cf19f7cf09f0b7471f39b6569a0562d566e0bbc
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-11 23:45:03 +0300

    task 3
    123

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,85 @@
-import throttle from 'lodash.throttle';
-import '../css/common.css';
 import '../css/03-feedback.css';
+import '../css/common.css';
+import throttle from 'lodash.throttle';
 
-const STOREGE_KEY = "feedback-form-state";
+const STORAGE_KEY = "feedback-form-state";
 
-const submit = document.querySelector(".feedback-form")
+const form = document.querySelector('.feedback-form');
 
-form.addEventListener("input", throttle(onInput, 500));
-submit.addEventListener("submit", onSubmit);
+
+
+form.addEventListener('input', throttle(onInput), 500)
+form.addEventListener('submit', onSubmit);
+
+
+initForm();
+updateInputForm();
 
 function onInput(evt) {
-    evt.preventDefault();
-    const message = evt.target.value;
-
-    localStorage.setItem(STOREGE_KEY, message);
+  formObject[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject));
 }
+
+function initForm() {
+  let persistedFilters = localStorage.getItem(STORAGE_KEY)
+  if (persistedFilters) {
+    persistedFilters = JSON.parse(persistedFilters);
+   Object.entries(persistedFilters).forEach (([name, value]) => {
+    formObject[name] = value;
+   form.elements[name].value = value;
+   });
+  }
+}
+
+
 
 function onSubmit(evt) {
-    evt.preventDefault();
-    const {
-        elements: { email, message },
-    } = evt.currentTarget;
-    const formData = { email: email.value, message: message.value };
-    console.log('тправка формы', formData);
-    evt.target.reset();
-    localStorage.removeItem(STOREGE_KEY)
+  evt.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  const formData = { email: email.value, message: message.value };
+
+  if (email.value === '' || message.value === '') {
+    return alert('Пожалуйста, заполните пустые строки!');
+  }
+  console.log("Отправляем форму: ", formData);
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
 }
 
-function populateTextarea() {
-    const saveMessagr = localStorage.getItem(STOREGE_KEY);
-    if (saveMessagr) {
+function updateInputForm() {
+ const saveForm = localStorage.getItem(STORAGE_KEY);
+ const saveFormPars = JSON.parse(saveForm);
+
+ if(saveForm) {
+  form.elements.email.value = saveFormPars.email || '';
+  form.elements.message.value = saveFormPars.message || '';
+ };
+};
+// function onInput(evt) {
+//     evt.preventDefault();
+//     const message = evt.target.value;
+
+//     localStorage.setItem(STORAGE_KEY, message);
+// }
+
+// function onSubmit(evt) {
+//     evt.preventDefault();
+//     const {
+//         elements: { email, message },
+//     } = evt.currentTarget;
+//     const formData = { email: email.value, message: message.value };
+//     console.log('отправка формы', formData);
+//     evt.target.reset();
+//     localStorage.removeItem(STORAGE_KEY)
+// }
+
+// function populateTextarea() {
+//     const saveMessagr = localStorage.getItem(STORAGE_KEY);
+//     if (saveMessagr) {
         
-    }
-}
+//     }
+// }
+
